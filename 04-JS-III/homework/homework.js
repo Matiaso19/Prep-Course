bf324f7c3d3237105a67f5db3d6e5b716b80b13d
Matias Olivera <matiasscrog@gmail.com>
2022-11-07 08:46:35 -0300

    falta poco

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -113,6 +113,16 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  for (let x = 0; x < arguments.length; x++) {
+    if (arguments.length > 1) {
+      x = arguments*arguments
+      return x
+    } else if (arguments.length === 0) {
+      return 0
+    } else if (arguments.length === 1) {
+      return arguments
+    }
+  }
 }
 
 
@@ -135,25 +145,29 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí  
-  switch (numeroDeDia) {
-    case '1':
-      return "Es fin de semana"
-    case '7': 
-      return "Es fin de semana"
-      break;
-    default: 
-      return "Es dia Laboral"
-      break;
-  } 
-  
-} 
+ if (numeroDeDia === 1) {
+  return "Es fin de semana"
+ } else if (numeroDeDia === 2) {
+  return "Es dia Laboral"
+ } else if (numeroDeDia === 3) {
+  return "Es dia Laboral"
+  }  else if (numeroDeDia === 4) {
+  return "Es dia Laboral"
+  } else if (numeroDeDia === 5) {
+    return "Es dia Laboral"
+  } else if (numeroDeDia === 6) {
+    return "Es dia Laboral"
+  } else if (numeroDeDia === 7) {
+    return "Es fin de semana"
+  }
+}
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
-  //Escribe tu código aquí
- 
+  //Escribe tu código aqu
+  return n.toString()[0] === "9"
 }
 
 
@@ -161,6 +175,7 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  
   
 } 
 
